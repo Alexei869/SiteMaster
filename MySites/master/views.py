ee7438966d4d79c562c5172b22f326423423cb2d
Alexei869 <bulatovak14@gmail.com>
2023-06-30 13:57:28 +0500

    added an authorization system

--- MySites/master/views.py
+++ MySites/master/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Пользователь 1',
-        'title': 'первая заявка',
-        'content': 'Содержание первой заявки.',
-        'date_posted': '12 июня, 2023'
-    },
-    {
-        'author': 'Пользователь 2',
-        'title': 'вторая заявка',
-        'content': 'Содержание второй заявки.',
-        'date_posted': '13 июня, 2023'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'master/home.html', context)
 
 
 def about(request):
-    return render(request, 'master/about.html', {'title': 'О сайте Master'})
+    return render(request, 'master/about.html', {'title': 'о сайте Master'})
